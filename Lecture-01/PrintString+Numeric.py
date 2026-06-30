e947a3a7ce73f7ea14cf95243315befa245c2038
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-06-30 15:35:25 +0700

    update code

---
+++ Lecture-01/PrintString+Numeric.py
@@ -0,0 +1,11 @@
+print("My age is", 19, "Ihave", 18.90,"Bth.")
+
+print("My age is %d and I have %.2f Bth." % (19,18.90))
+
+print("My age is " + str(19)+ " and I have " + str(18.90) + " Bth.")
+
+print("5+4 =", 5+4)
+
+print("5+4 = %.2f" % (5+4))
+
+print("5+4 = " + str(5+4))
---
+++ Lecture-01/main.py
@@ -0,0 +1,9 @@
+print("Hello")
+print("Information and Network Engineering")
+print("Hello Pakin you will be great")
+
+print("I'm hungry")
+print('He ask me "Who got the book?"')
+
+print("I'm good and \"I will do my best\"")
+print("/\\")
