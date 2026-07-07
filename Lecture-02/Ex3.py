72384427e290071b15b5e9629bb1e9310e6de2ee
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-07 08:23:50 +0000

    Jul 7, 2026, 3:23 PM

--- Lecture-02/Ex3.py
+++ Lecture-02/Ex3.py
@@ -3,4 +3,4 @@ Celsius = int(input("Please enter the temperature in Celsius: "))
 Celsius = int(Celsius)  # Ensure Celsius is an integer
 
 Fahrenheit = (Celsius * 9/5) + 32  # Convert Celsius to Fahrenheit
-print("Temperature in Fahrenheit is:", Fahrenheit)
+print("Temperature in Fahrenheit is:", format(Fahrenheit, ".2f"))
