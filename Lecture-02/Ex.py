21a4c5e2888c66900a68baff1ff1bacd88d93424
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-07 15:11:12 +0700

    commit

---
+++ Lecture-02/Ex.py
@@ -0,0 +1,9 @@
+# Getting user input and converting to int
+age = int(input("Please enter your age: "))
+age = int(age)  # Ensure age is an integer
+
+# Getting user input and converting to float
+height = float(input("Please enter your height: "))
+height = float(height)  # Ensure height is a float
+
+print("You are" + str(age) + " years old and " + str(height) + " feet tall.")
