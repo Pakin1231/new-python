d739e866651e512eae1277f97f11f789e3060bc3
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-07 07:33:26 +0000

    Jul 7, 2026, 2:33 PM

--- Lecture-02/TestTypeConv.py
+++ Lecture-02/TestTypeConv.py
@@ -1,3 +1,3 @@
 first_name = input("Pakin: ")
 last_name = input("Jaruwannangkul: ")
-print("Hello " + first_name + " " + last_name)
+print("Hello " + first_name + " " + last_name + "!Welcome to Python programming!")
