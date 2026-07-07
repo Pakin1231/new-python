e6af77cdea5da9c7eaf4f116a9333f643152a46a
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-07 08:19:00 +0000

    Jul 7, 2026, 3:19 PM

---
+++ Lecture-02/Ex3.py
@@ -0,0 +1,6 @@
+# Getting user input and converting to int
+Celsius = int(input("Please enter the temperature in Celsius: "))
+Celsius = int(Celsius)  # Ensure Celsius is an integer
+
+Fahrenheit = (Celsius * 9/5) + 32  # Convert Celsius to Fahrenheit
+print("Temperature in Fahrenheit is:", Fahrenheit)
