2db7bcac5d5c62f79c8c4ff5c7937b875b9a2489
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 06:43:59 +0000

    Jul 14, 2026, 1:43 PM

--- Lecture-03/Ex1.py
+++ Lecture-03/Ex1.py
@@ -2,4 +2,5 @@ Score1 = int(input("Enter your 1 test Score: "))
 Score2 = int(input("Enter your 2 test Score: "))
 Score3 = int(input("Enter your 3 test Score: "))
 Output = (Score1 + Score2 + Score3) / 3
+if Output < 95: print("The average score is")
 if Output > 95: print("Congratulations!")
