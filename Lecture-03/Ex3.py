6df768ff16400bdea99975deac192d305537ba91
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 08:16:35 +0000

    Jul 14, 2026, 3:16 PM

--- Lecture-03/Ex3.py
+++ Lecture-03/Ex3.py
@@ -1,5 +1,5 @@
 payrate = 20
-num = float(input("Enter the number of hours worked"))
+num = float(input("Enter the number of hours worked: "))
 if num > 40:
 	pay = payrate * 40 + (num - 40) * payrate * 1.5
 else:
