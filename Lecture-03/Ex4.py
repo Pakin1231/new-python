23fec7942cf4f30fcc03d206721d283dc3c0bb36
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 16:05:01 +0700

    commit

--- Lecture-03/Ex4.py
+++ Lecture-03/Ex4.py
@@ -9,13 +9,13 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 if choice == 1:
-    print(num1 + num2)
+    print(f"{num1} + {num2} = {num1 + num2}")
 
 elif choice == 2:
-    print(num1 - num2)
+    print(f"{num1} - {num2} = {num1 - num2}")
 
 elif choice == 3:
-    print(num1 * num2)
+    print(f"{num1} * {num2} = {num1 * num2}")
 
 elif choice == 4:
-    print(num1 / num2)
+    print(f"{num1} / {num2} = {num1 / num2}")
