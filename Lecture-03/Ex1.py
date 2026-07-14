bc254aa0134b861fc3ff6b5beee4299d9538fd78
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 06:37:08 +0000

    Jul 14, 2026, 1:37 PM

---
+++ Lecture-03/Ex1.py
@@ -0,0 +1,5 @@
+Score = int(input("Enter your 1 test Score: "))
+Score = int(input("Enter your 2 test Score: "))
+Score = int(input("Enter your 3 test Score: "))
+if Score < 95:
+    print("Congratulations!")
