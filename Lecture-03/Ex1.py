4502eda924cca149975091b7593606c7a69a5d55
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 06:39:13 +0000

    Jul 14, 2026, 1:39 PM

--- Lecture-03/Ex1.py
+++ Lecture-03/Ex1.py
@@ -1,5 +1,6 @@
-Score = int(input("Enter your 1 test Score: "))
-Score = int(input("Enter your 2 test Score: "))
-Score = int(input("Enter your 3 test Score: "))
-if Score < 95:
+Score1 = int(input("Enter your 1 test Score: "))
+Score2 = int(input("Enter your 2 test Score: "))
+Score3 = int(input("Enter your 3 test Score: "))
+Output = (Score1 + Score2 + Score3) / 3
+if Output > 95:
     print("Congratulations!")
