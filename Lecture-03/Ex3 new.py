6f5039e4068264d466d064b4f4458c20836c937a
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-07-14 09:19:54 +0000

    Jul 14, 2026, 4:19 PM

--- Lecture-03/Ex3 new.py
+++ Lecture-03/Ex3 new.py
@@ -1,5 +1,8 @@
-work_hour = int(input("Enter the number of hours worked"))
-hourly_rate = float(input("Enter the number of hourly rate"))
+work_hour = int(input("Enter the number of hours worked: "))
+hourly_rate = float(input("Enter the number of hourly rate: "))
 #if work_hour > 40:
-pay = (work_hour - 40) * hourly_rate * 1.5 + (40 * hourly_rate)
+if work_hour <= 40:
+    pay = work_hour * hourly_rate
+else:
+    pay = (work_hour - 40) * hourly_rate * 1.5 + (40 * hourly_rate)
 print("Total pay for the week: $", format(pay,".2f"))
