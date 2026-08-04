75d48ad6959a7ce876e87ca0e39218f88bf877e6
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-04 06:42:28 +0000

    Aug 4, 2026, 1:42 PM

---
+++ Lecture-05/functions with parameters1.py
@@ -0,0 +1,8 @@
+def greet(name="World"):
+    print(f"Hello, {name}!")
+
+# Calling the function without an argument
+greet() #Output: Hello, World!
+
+# Calling the function with an argument
+greet("Pakin") #Out put: Hello, Pakin!
