feb3b2362831211b81b5bcf7d242c485ec2821e5
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-04 06:35:41 +0000

    Aug 4, 2026, 1:35 PM

---
+++ Lecture-05/functions3.py
@@ -0,0 +1,15 @@
+#This program has two functions.
+#Frist we define the main function.
+
+def main():
+    print("I have a message for you")
+    message()
+    print("Goodbye!")
+
+#Next we define the message function.
+def message():
+    print("I am Pakin")
+    print("I Love Python")
+
+#Call the main functions.
+main()
