2b5816f31e0421286ed280f5e2f3ebd9a1cc399e
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-11 06:51:31 +0000

    Aug 11, 2026, 1:51 PM

---
+++ Lecture-06/list methods(insert).py
@@ -0,0 +1,6 @@
+# Insert method
+berries = ["raspberry", "blackberry"]
+berries.insert(1, "strawberry")
+berries.insert(2, "blueberry")
+print(f"Berries after insert: {berries}")
+# Output: Berries after insert: ['respberry', 'strawberry', 'blueberry', 'blackberry']
