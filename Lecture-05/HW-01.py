2ec3cc17fd44d125493f440418abdadef5a066a1
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-11 06:04:49 +0000

    Aug 11, 2026, 1:04 PM

--- Lecture-05/HW-01.py
+++ Lecture-05/HW-01.py
@@ -1,20 +1,13 @@
-def format_strings(*args, sep=''):
-    """
-    This function takes multiple string arguments, concatenates them using an
-    optional separator, and returns the result in uppercase.
-    
-    :param args: Variable length string arguments
-    :param sep: Optional separator inserted between provided strings
-    :return: Concatenated uppercase string
-    """
-    return sep.join(args).upper()
+def format_strings(*args):
+    return "".join(arg.replace(" ", "-").upper() for arg in args)
+    pass
 
-result = format_strings("Hello", "World", "this", "is", "a", "test")
-print(result)
+if __name__ == '__main__':
+    result = format_strings("Hello", "world", "this", "is", "a", "test")
+    print(result)  # Output: "HELLOWORLDTHISISATEST"
 
-result = format_strings("Python", "is", "fun")
-print(result)
+    result = format_strings("Python", "is", "fun")
+    print(result)  # Output: "PYTHONISFUN"
 
-#(ส่ง sep='-' เข้าไป)
-result = format_strings("Hello", "World", sep="-")
-print(result)
+    result = format_strings("Hello world")
+    print(result)  # Output: "HELLO-WORLD"
