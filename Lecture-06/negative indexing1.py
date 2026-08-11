32a07e5ba903c5eca357fd06e8f71b92c158826c
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-11 06:40:11 +0000

    Aug 11, 2026, 1:40 PM

--- Lecture-06/negative indexing1.py
+++ Lecture-06/negative indexing1.py
@@ -3,3 +3,5 @@ second_to_last_color = colors[-2]
 print(f"Second to last color: {second_to_last_color}")
 third_to_last_color = colors[-3]
 print(f"third to last color: {third_to_last_color}")
+last_color = colors[len (colors -1)]
+print(f"last color: {last_color}")
