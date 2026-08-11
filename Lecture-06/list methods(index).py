5faa6753a434c133c12006d2676034c75aed2926
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-08-11 07:05:49 +0000

    Aug 11, 2026, 2:05 PM

---
+++ Lecture-06/list methods(index).py
@@ -0,0 +1,10 @@
+# Using index() to find the first occurence of "dog"
+animals = ["cat", "dog", "rabbit", "hamster", "dog", "parrot"]
+first_dog_index = animals.index("dog")
+print(f"The first occurrence of 'dog' is at index: {first_dog_index}")
+# Output: The first occurence of 'dog' is at index: 1
+
+# Using index() to find the second occurrence of "dog"
+second_dog_index = animals.index("dog", first_dog_index + 1)
+print (f"The second occurrence of 'dog' is at index: {second_dog_index}")
+#Output: The second occurrence of 'dog' is at index: 4
