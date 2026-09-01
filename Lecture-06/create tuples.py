ca3a437b9e27c11e2f7e880c69527b473994199d
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-01 06:47:46 +0000

    Sep 1, 2026, 1:47 PM

---
+++ Lecture-06/create tuples.py
@@ -0,0 +1,3 @@
+# Creating a tuple
+my_tuple = (1, 2, 3, "apple", "banana")
+print(my_tuple)
--- Lecture-06/two-dimensional lists2.py
+++ Lecture-06/two-dimensional lists2.py
@@ -8,15 +8,15 @@ COLS = 4
 
 def main():
 #Create a two-dimensional list.
-values = [[0, 0, 0, 0], 
-           [0, 0, 0, 0], 
-           [0, 0, 0, 0]]
-#Fill the list with random numbers.
-for r in range(ROWS):
-    for c in range(COLS):
-        values [r] [c] = random.randint(1, 100)
+    values = [[0, 0, 0, 0], 
+              [0, 0, 0, 0], 
+              [0, 0, 0, 0]]
+    #Fill the list with random numbers.
+    for r in range(ROWS):
+        for c in range(COLS):
+            values [r] [c] = random.randint(1, 100)
     #Display the random numbers.
     print(values)
-    
+
 #Call the main function.
 main()
