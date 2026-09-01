79ad49562623e51c1cc744cea7eab7173620daa4
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-01 07:27:42 +0000

    Sep 1, 2026, 2:27 PM

--- Lecture-08/readsale.py
+++ Lecture-08/readsale.py
@@ -2,3 +2,9 @@ with open('sales.txt', 'r') as sales_file:
     for line in sales_file:
         amount = float(line)
         print(format(amount, '.2f'))
+
+#with open('sales.txt', 'r') as sales_file:
+#   while in sales_file:
+#        amount = float(line)
+#        print(format(amount, '.2f'))
+#       line = sales_file.readline()
