86daeb5d705e58df3f3541ef141feeb3c3b33367
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-08 08:38:33 +0000

    Sep 8, 2026, 3:38 PM

---
+++ Lecture-07/access_Dic_loop.py
@@ -0,0 +1,10 @@
+student = {"name": "Alice", "age": 25, "grade": "A", "major": "Computer Science"}
+
+for key in student:
+    print(f"{key}: {student[key]}")
+
+#Output:
+# name: Alice
+# age: 25
+# grade: A
+#major: Computer Science
