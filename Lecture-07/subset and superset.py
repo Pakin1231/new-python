5ffac15d63f2879f6c9a507c89d4e5618065a82d
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-08 07:15:29 +0000

    Sep 8, 2026, 2:15 PM

--- Lecture-07/subset and superset.py
+++ Lecture-07/subset and superset.py
@@ -7,5 +7,12 @@ set_d = {1, 2, 3, 4, 5}
 print("is set_a a superset of set_b?", set_a >= set_b)  # Output: True
 print("is set_b a subset of set_a?", set_b <= set_a)  # Output: True
 
-print("is set_a a superset of set_b?", set_a > set_b)  # Output: True
-print("is set_b a subset of set_a?", set_b < set_a)  # Output: True
+#Proper superset and subset
+print("is set_a a proper superset of set_b?", set_a > set_b)  # Output: True
+print("is set_b a proper subset of set_a?", set_b < set_a)  # Output: True
+
+#Equal Sets
+print("is set_a equal to set_c?", set_a == set_c)  # Output: True
+
+#Relation but not equal (one is subset but not equal)
+print("is set_b a superset of set_d and not equal?", set_b <= set_d and set_b != set_d)  # Output: True
