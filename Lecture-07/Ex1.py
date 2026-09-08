2f8b6fbae152620a9fb418d98f1eecd69b06ebc2
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-08 07:41:04 +0000

    Sep 8, 2026, 2:41 PM

--- Lecture-07/Ex1.py
+++ Lecture-07/Ex1.py
@@ -13,3 +13,23 @@ print("1. Chosen by all:", common_langs)
 
 only_p1 = sets[0] - sets[1]
 print("2. Only chosen by P1 (not P2):", only_p1)
+
+all_unique_langs = set.union(*sets)
+print("3. Number of unique languages:", len(all_unique_langs))
+
+either_p1_p2 = sets[0] ^ sets[1]
+print("4. Chosen by P1 or P2, but not both:", either_p1_p2)
+
+same_choices = []
+for i in range(len(sets)):
+    for j in range(i + 1, len(sets)):
+        if sets[i] == sets[j]:
+            same_choices.append((f"Participant {i+1}", f"Participant {j+1}"))
+
+same_choices = []
+for i in range(len(sets)):
+    for j in range(i + 1, len(sets)):
+        if sets[i] == sets[j]:
+            same_choices.append((f"Participant {i+1}", f"Participant {j+1}"))
+
+print("5. Identical choices:", same_choices)
