d6eae0c0d7289663ed5a6f39dddf1b4417c167d9
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-08 07:13:10 +0000

    Sep 8, 2026, 2:13 PM

---
+++ Lecture-07/subset and superset.py
@@ -0,0 +1,11 @@
+set_a = {1, 2, 3, 4}
+set_b = {2, 3}
+set_c = {1, 2, 3, 4}
+set_d = {1, 2, 3, 4, 5}
+
+#Superset and subset
+print("is set_a a superset of set_b?", set_a >= set_b)  # Output: True
+print("is set_b a subset of set_a?", set_b <= set_a)  # Output: True
+
+print("is set_a a superset of set_b?", set_a > set_b)  # Output: True
+print("is set_b a subset of set_a?", set_b < set_a)  # Output: True
