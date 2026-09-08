b2b7a300ed2510de232e59266aa4f8c4271bb4e6
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-08 09:10:54 +0000

    Sep 8, 2026, 4:10 PM

---
+++ Lecture-07/dicmethods.py
@@ -0,0 +1,14 @@
+phonebook = {'Anirah': '777-1111', 'Mickey': '777-2222', 'Donald': '777-3333', 'Pluto': '777-4444'}
+
+heroesdict = {}
+heroesdict['Hulk'] = '888-1111'
+heroesdict['Ironman'] = '888-2222'
+print(heroesdict.get('Hulk', 'Key not found'))  # Output: 888-1111
+print(heroesdict.get('Halk', 'Key not found'))  # Output: Key not found
+
+for key, value in phonebook.items():
+    print("{key}: {value}")
+
+print(phonebook.keys())  # Output: dict_keys(['Anirah', 'Mickey', 'Donald', 'Pluto'])
+print(phonebook.values())  # Output: dict_values(['777-1111', '777-2222', '777-3333', '777-4444'])
+print(phonebook.pop('Mickey', 'Element not found'))  # Output: 777-2222
