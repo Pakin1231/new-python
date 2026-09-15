3cee69e70f7feb4d4e430f9b583200443d741eee
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-15 07:09:05 +0000

    Sep 15, 2026, 2:09 PM

--- Lecture-09/try_else block.py
+++ Lecture-09/try_else block.py
@@ -1,9 +1,6 @@
-def devide(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError as e:
-        print("Exception: ", e)
-    else:
-        return result
+def divide(a,b):
+    return a/b
 
-a,b = map(int, input().split())
+a,b = map(int, input().split)
+print(divide(a,b))
+print("End of program")
