0755806d8d6c363727f7e71562a1869572b7a821
Pakin Jaruwannangkul <ponpakin3@gmail.com>
2026-09-22 06:24:47 +0000

    Sep 22, 2026, 1:24 PM

---
+++ Lecture-10/OS Module.py
@@ -0,0 +1,6 @@
+import os
+
+print (os.name) # Output: posix (on Unix-like systems), nt (on Windows)
+print (os. getcwd ()) # Output: Current working directory
+os. mkdir("new_directory") # Create a new directory
+
